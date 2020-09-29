aed94c337caf241f53ae943ad9c4387ee8408574
dolores-aw <65312905+dolores-aw@users.noreply.github.com>
2020-09-29 09:23:46 -0500

    Update runningModels.sh

--- sbatch_scripts/runningModels.sh
+++ sbatch_scripts/runningModels.sh
@@ -10,5 +10,5 @@ EMULATOR_BASE_DIR=~
 mkdir -p $TRAINING_DIR $SAVE_DIR $PLOT_DIR
 echo "TRAINING MODEL on $(date), logging to ${TRAINING_LOG}"
 echo "Running train_and_save_main!" >> $TRAINING_LOG
-python $EMULATOR_BASE_DIR/trainhyperparam.py --save-loc $SAVE_DIR --data-loc $DATA_DIR/burgers_shock.mat --base-plot-dir $PLOT_DIR --lossplot-loc $PLOT_DIR/loss_plot.eps --epochs 10000  >> $TRAINING_LOG 2>&1
+python $EMULATOR_BASE_DIR/train_model.py --save-loc $SAVE_DIR --data-loc $DATA_DIR/burgers_shock.mat --base-plot-dir $PLOT_DIR --lossplot-loc $PLOT_DIR/loss_plot.eps --epochs 10000  >> $TRAINING_LOG 2>&1
 echo "train_and_save_main finished with return code $?." >> $TRAINING_LOG
