162ea7a7b96c653b4a85feaca5432d4cbec3a7bb
dolores-aw <65312905+dolores-aw@users.noreply.github.com>
2020-10-19 17:27:08 -0500

    Add files via upload

--- selected_data/parameter_search/find_parameter.py
+++ selected_data/parameter_search/find_parameter.py
@@ -15,20 +15,20 @@ typen = 'N_f'
 m = 0.1
 ntrials = 20
 
-lam_list = [0.25]#[0.01, 0.1, 0.5, 0.25, 1, 1.25, 1.5, 1.1, 1.01]
+lam_list = [0.01, 0.1, 0.5, 0.25, 1, 1.25, 1.5, 1.1, 1.01]
 
-if not os.path.isfile('param_dict_%s_%s_%s_%s_%s.p' % (N_u,N_f,N_u,N_f2,typen)):
-    with open('param_dict_%s_%s_%s_%s_%s.p' % (N_u,N_f,N_u2,N_f2,typen), 'wb') as fp:
-        pickle.dump({}, fp,protocol=2)
-
+#if not os.path.isfile('param_dict_%s_%s_%s_%s_%s.p' % (N_u,N_f,N_u,N_f2,typen)):
+#    with open('param_dict_%s_%s_%s_%s_%s.p' % (N_u,N_f,N_u2,N_f2,typen), 'wb') as fp:
+#        pickle.dump({}, fp,protocol=2)
+inputs = interiorburgerslambda.prepare_nn_inputs_burgers('burgers_shock.mat', N_u, N_f, N_u2, N_f2, m, typen, debugging=False)
 for lam in lam_list:
     errors = []
-    for trial in range(0,ntrials):
-        error = train_find_parameter.training(N_u, N_f, N_u2, N_f2, typen, m, lam)
-        errors.append(error)
+    #for trial in range(0,ntrials):
+    error = train_find_parameter.training(inputs, N_u,N_f,N_u2,N_f2, typen, m, lam)
+    errors.append(error)
     with open('param_dict_%s_%s_%s_%s_%s.p'% (N_u,N_f,N_u2,N_f2,typen), 'rb') as fp:
         d = pickle.load(fp)
-    d[lam] = errors
+    d[lam] = d[lam] + errors
     with open('param_dict_%s_%s_%s_%s_%s.p' % (N_u,N_f,N_u2,N_f2,typen), 'wb') as fp:
         pickle.dump(d, fp, protocol=2)
 
