e271a575a52ac45947e32d30854e1d121eab7e1f
dolores-aw <65312905+dolores-aw@users.noreply.github.com>
2020-10-20 08:21:31 -0500

    Add files via upload

---
+++ parameter_search/currentmethod/scripts/runningModels2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TRAINING_DIR=~/modelTrainLogs/
+SAVE_DIR=~/junk/eg_model
+DATA_DIR=~/data
+PLOT_DIR=~/plots
+TRAINING_LOG=$TRAINING_DIR/$(date +"%Y_%m_%d_%I_%M_%p")_$(hostname).log
+EMULATOR_BASE_DIR=~
+
+mkdir -p $TRAINING_DIR $SAVE_DIR $PLOT_DIR
+echo "TRAINING MODEL on $(date), logging to ${TRAINING_LOG}"
+echo "Running train_and_save_main!" >> $TRAINING_LOG
+python $EMULATOR_BASE_DIR/find_parameter.py --save-loc $SAVE_DIR --data-loc $DATA_DIR/burgers_shock.mat --base-plot-dir $PLOT_DIR --lossplot-loc $PLOT_DIR/loss_plot.eps --epochs 5000 >> $TRAINING_LOG 2>&1
+echo "train_and_save_main finished with return code $?." >> $TRAINING_LOG
